38e54179794221f8505fbc07d03c641b34665f85
Loïc Marchand <nilpa018@yahoo.fr>
2024-10-02 14:46:12 +0200

    feat(main): creating project architecture and connectivity to local mysqlDB

---
+++ src/main/java/org/example/tpcrm2i/Tpcrm2iApplication.java
@@ -0,0 +1,13 @@
+package org.example.tpcrm2i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpcrm2iApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tpcrm2iApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tpcrm2i/Tpcrm2iApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tpcrm2i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpcrm2iApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
