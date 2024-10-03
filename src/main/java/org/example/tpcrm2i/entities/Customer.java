5b966171f12c239fb73f7d4847ebcd023f247c78
Loïc Marchand <nilpa018@yahoo.fr>
2024-10-03 15:41:36 +0200

    feat(FirstConnectAPI): improve Customer Entity with relationnal database connection

--- src/main/java/org/example/tpcrm2i/entities/Customer.java
+++ src/main/java/org/example/tpcrm2i/entities/Customer.java
@@ -1,8 +1,11 @@
 package org.example.tpcrm2i.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -12,26 +15,41 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
-    @Column(name = "company_name")
+
+    @Column(name = "company_name", nullable = false, length = 100)
     String company_name;
-    @Column(name = "first_name")
+
+    @Column(name = "first_name", nullable = false, length = 50)
     String first_name;
-    @Column(name = "last_name")
+
+    @Column(name = "last_name", nullable = false, length = 50)
     String last_name;
-    @Column(name = "email")
+
+    @Column(name = "email", nullable = false, length = 100)
     String email;
-    @Column(name = "phone_number")
+
+    @Column(name = "phone_number", unique = true, length = 15)
     String phone_number;
-    @Column(name = "address")
+
+    @Column(name = "address", nullable = false)
     String address;
-    @Column(name = "zip_code")
+
+    @Column(name = "zip_code", nullable = false, length = 30)
     String zip_code;
-    @Column(name = "country")
+
+    @Column(name = "country", nullable = false, length = 100)
     String country;
-    @Column(name = "city")
+
+    @Column(name = "city", nullable = false, length = 100)
     String city;
+
     @Column(name = "state")
     int state;
+
+    @Getter
+    @OneToMany(mappedBy = "customer", fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<Order> orders = new HashSet<>();
 
     public Customer(String company_name, String first_name, String last_name, String email, String phone_number, String address, String zip_code, String country, String city, int state) {
         this.company_name = company_name;
